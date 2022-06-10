b90077caaa7ab96f141ea89f57121edc7f98c492
frantecbh <franciscomenezesbh@gmail.com>
2022-06-10 16:46:46 -0300

    estrutura dashboard+sumary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,12 +4,13 @@ export const GlobalStyle = createGlobalStyle`
     :root{
       --background: #f0f2f5;
       --red: #E52E4D;
+      --green: #33cc95;
       --blue: #5429CC;
       
       --blue-ligth: #6933FF;
       --text-title: #363F5F;
       --text-body: #969CB3;
-      --shpe: #FFFFFF;
+      --shape: #FFFFFF;
     }
     *{
       margin: 0;
