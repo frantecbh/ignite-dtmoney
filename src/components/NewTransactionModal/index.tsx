fe77a4959d79f0fc711ea04b50d74ea972f7f73d
frantecbh <franciscomenezesbh@gmail.com>
2022-06-14 19:18:15 -0300

    pegando valores dos inputs

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { FormEvent, useState } from 'react'
 
 import Modal from 'react-modal'
 import { Container, TransactionTypeContainer, RadioBox } from './style';
@@ -18,6 +18,23 @@ interface NewTransactionModalProps {
 export default function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
 
     const [type, setType] = useState('deposit')
+    const [title, setTitle] = useState('')
+    const [value, setValue] = useState(0)
+    const [category, setCategory] = useState('')
+
+
+    function handleCreateNewTransaction(event: FormEvent) {
+
+        event.preventDefault()
+
+        console.log({
+            title,
+            value,
+            category,
+            type
+        })
+
+    }
 
 
     return (
@@ -38,12 +55,22 @@ export default function NewTransactionModal({ isOpen, onRequestClose }: NewTrans
             </button>
 
 
-            <Container>
+            <Container onSubmit={handleCreateNewTransaction}>
                 <h2>Cadastrar transação</h2>
 
-                <input type="text" placeholder='Título' />
+                <input
+                    type="text"
+                    placeholder='Título'
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}
+                />
 
-                <input type="number" placeholder='Valor' />
+                <input
+                    type="number"
+                    placeholder='Valor'
+                    value={value}
+                    onChange={event => setValue(Number(event.target.value))}
+                />
 
                 <TransactionTypeContainer>
                     <RadioBox
@@ -67,7 +94,12 @@ export default function NewTransactionModal({ isOpen, onRequestClose }: NewTrans
 
                 </TransactionTypeContainer>
 
-                <input type="text" placeholder='Categoria' />
+                <input
+                    type="text"
+                    placeholder='Categoria'
+                    value={category}
+                    onChange={event => setCategory(event.target.value)}
+                />
 
                 <button type='submit'>Cadastrar</button>
             </Container>
